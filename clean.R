e30fa209bcceb5ae27d6ae5f9cc7a5cbf0f8b259
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-11 00:17:38 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -5,4 +5,4 @@ raw_data <- raw_data[-1,]
 raw_data <- raw_data[5:8] 
 colnames(raw_data)<-c("statename", "population", "popover18", "percentover18") #renaming the columns to stateName, population, popOver18 and percentOver18
 clean_data <- raw_data
-View(clean_data)
+
